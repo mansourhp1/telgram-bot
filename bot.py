6776d430d3816cbd27e519ca06b5ac07d59961b6
mansourhp1 <mansour2hajipoor@gmail.com>
2025-11-29 12:00:37 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! من آنلاینم :)")
 
 if _name_ == '__main__':
+    main()
     app = ApplicationBuilder().token("8336683539:AAEKR-IcSArq_9trWRSq7uTQAnoka_M696k").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
