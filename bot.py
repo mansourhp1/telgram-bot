949ed1809de00733f6d1e5b8745af8ffbc3eaf37
mansourhp1 <mansour2hajipoor@gmail.com>
2025-11-29 11:34:06 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,7 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام! من آنلاینم :)")
 
-if name == '__main__':
+if _name_ == '__main__':
     app = ApplicationBuilder().token("8336683539:AAEKR-IcSArq_9trWRSq7uTQAnoka_M696k").build()
     app.add_handler(CommandHandler("start", start))
     app.run_polling()
